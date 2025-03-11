a7fd5016f68956edb2f556f7b1f11f9d17f6a582
jamesLaurino <petersthomas90@gmail.com>
2025-03-11 16:29:36 +0100

    FTO-0064 check with email before sign up (register)

--- src/main/java/com/fotova/service/email/EmailService.java
+++ src/main/java/com/fotova/service/email/EmailService.java
@@ -11,6 +11,16 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    public void sendRegisterEmail(String uuid) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("test@example.com");
+        message.setTo("destinataire@example.com");
+        message.setSubject("Email send from Fotova-creation for register");
+        message.setText("Click here : http://localhost:8080/api/v1/auth/register/check?uuid=" + uuid);
+
+        mailSender.send(message);
+    }
+
     public void sendOrderEmail(String orderId) {
 
         SimpleMailMessage message = new SimpleMailMessage();

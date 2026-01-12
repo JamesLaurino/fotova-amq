71e19f84c660ccaf0a990bb40e22e59b65dff44b
jamesLaurino <petersthomas90@gmail.com>
2026-01-12 21:17:59 +0100

    FTO-0002 refactoring env variables

--- src/main/java/com/fotova/service/email/EmailService.java
+++ src/main/java/com/fotova/service/email/EmailService.java
@@ -2,6 +2,7 @@ package com.fotova.service.email;
 
 import com.fotova.dto.ContactDtoAmq;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -12,12 +13,21 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${app.env.host}")
+    private String HOST;
+
+    @Value("${app.env.protocol}")
+    private String PROTOCOL;
+
+    @Value("${app.env.sender.email}")
+    private String SENDER_EMAIL;;
+
     public void sendRegisterEmail(String uuid) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("test@example.com");
         message.setTo("destinataire@example.com");
         message.setSubject("Email send from Fotova-creation for register");
-        message.setText("Click here : http://localhost:8080/api/v1/auth/register/check?uuid=" + uuid);
+        message.setText("Click here : " + PROTOCOL + "://" + HOST + ":8080/api/v1/auth/register/check?uuid=" + uuid);
 
         mailSender.send(message);
     }
@@ -26,7 +36,7 @@ public class EmailService {
 
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(contactDtoAmq.getEmail());
-        message.setTo("fotova-creation@example.com");
+        message.setTo(SENDER_EMAIL);
         message.setSubject(contactDtoAmq.getSujet());
         message.setText("Message en provenance du user : " + contactDtoAmq.getNom() + ".\n" + contactDtoAmq.getMessage());
         mailSender.send(message);
